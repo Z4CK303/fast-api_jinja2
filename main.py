0d85b0ada4a8366bcb08dd84fa700b5c9ddb86a9
jeda <je.da.gr.p@gmail.com>
2026-05-09 00:45:30 -0500

    hola

---
+++ main.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def inicio(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="inicio.html",
+    )
+
+
+@app.get("/zero-mission.html", response_class=HTMLResponse)
+async def zero_mission(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="zero-mission.html",
+    )
+
+
+@app.get("/samus-returns.html", response_class=HTMLResponse)
+async def samus_returns(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="samus-returns.html", 
+    )
+
+
+@app.get("/super-metroid.html", response_class=HTMLResponse)
+async def super_metroid(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="super-metroid.html",
+    )
